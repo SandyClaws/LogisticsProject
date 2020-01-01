32b385e4dac861a85399fa6ead315c85a7cfc9b6
SandyClaws <usanna1311@gmail.com>
2020-01-01 10:58:03 +0700

    Убран дублирующий блок в меню

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,5 +1,10 @@
-$('.mobile-menu_btn').on('click', function(){
+/*$('.mobile-menu_btn').on('click', function(){
 $('.mobile-menu_btn').toggleClass("active-menu-btn");
 $('.active-mobile-menu').toggle();
 $('.header-phone__item').toggle();
-});
+});*/
+$('.header-menu_btn').on('click', function(){
+  $('.header-phone').toggleClass("active-menu");
+  $('.header-menu_btn').toggleClass("cl-btn");
+  $('.header-menu').toggle();
+  });
