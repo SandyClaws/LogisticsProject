3fe12f1fd50eca7552ab7a1f29eb0adcb8c78424
SandyClaws <usanna1311@gmail.com>
2020-01-02 18:04:09 +0700

    4 шаг слайдер

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,10 +1,6 @@
-/*$('.mobile-menu_btn').on('click', function(){
-$('.mobile-menu_btn').toggleClass("active-menu-btn");
-$('.active-mobile-menu').toggle();
-$('.header-phone__item').toggle();
-});*/
 $('.header-menu_btn').on('click', function(){
   $('.header-phone').toggleClass("active-menu");
+  $('.header-phone').toggleClass("view-phone");
   $('.header-menu_btn').toggleClass("cl-btn");
   $('.header-menu').toggle();
   });
